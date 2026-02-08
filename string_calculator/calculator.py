bee87c9a535e42890f22e0b76edf3c25cb06efea
gourav005 <gouravthakur0000@gmail.com>
2026-02-08 20:06:24 +0530

    test : deliemter

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -3,7 +3,13 @@ def add(numbers: str) -> int:
     if numbers == '':
         return 0
 
-    numbers = numbers.replace("\n", ",")
-    parts = numbers.split(",")
+    delimiter = ","
+
+    if numbers.startswith("//"):
+        header, numbers = numbers.split("\n", 1)
+        delimiter = header[2:]
+
+    numbers = numbers.replace("\n", delimiter)
+    parts = numbers.split(delimiter)
 
     return sum(int(part) for part in parts)
